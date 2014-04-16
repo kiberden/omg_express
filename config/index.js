cf8047960b5838b8c4c27b19ee85cf7b682e1812
kiberden <volgodenchik@gmail.com>
2014-04-17 00:57:44 +0400

    Revert "test"
    This reverts commit 81bc2351a5b4f4d0f92cc92e03dc69fdf10e8122.

--- config/index.js
+++
@@ -1,8 +0,0 @@
-var nconf = require('nconf');
-var path = require('path');
-
-nconf.argv()
-    .env()
-    .file({ file: path.join(__dirname, "config.json" )});      //задаем пусть, откуда читать фаил конфигураций
-                                                               //имя дериктории + имя фаила
-module.exports = nconf;
--- config/schema.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var config = require('config');
-
-mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'));    //задаем конфигурации mongoose, считывая их из фаила
-
-module.exports = mongoose;
